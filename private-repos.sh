fa03d09d62b8f2094b7946b66d092f3e9a211d31
Adones Pitogo <pitogo.adones@gmail.com>
2023-06-29 07:39:58 +0800

    added basic-net-mgr plugin

--- private-repos.sh
+++ private-repos.sh
@@ -9,4 +9,5 @@ export GOPRIVATE="$GOPRIVATE,$ROOT_REPO/default-theme"
 export GOPRIVATE="$GOPRIVATE,$ROOT_REPO/wifi-hotspot"
 export GOPRIVATE="$GOPRIVATE,$ROOT_REPO/wired-coinslot"
 export GOPRIVATE="$GOPRIVATE,$ROOT_REPO/basic-system-account"
+export GOPRIVATE="$GOPRIVATE,$ROOT_REPO/basic-net-mgr"
 
