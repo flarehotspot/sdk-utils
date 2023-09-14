a3c0d9c1f98d1a251cde28396424da4bea75df8b
Adones Pitogo <pitogo.adones@gmail.com>
2023-09-14 16:09:49 +0800

    can specify custom go path

--- install-go.sh
+++ install-go.sh
@@ -8,36 +8,39 @@ GOARCH=$(go env GOARCH)
 GO_VERSION=$(cat "${WORKDIR}/go-version")
 GO_TAR="go${GO_VERSION}.${GOOS}-${GOARCH}.tar.gz"
 GO_SRC="https://go.dev/dl/${GO_TAR}"
-GO_PATH="${WORKDIR}/go"
+GO_CUSTOM_PATH="${GO_CUSTOM_PATH:-${WORKDIR}/go}"
 DL_PATH="${CACHE_PATH}/downloads/${GO_TAR}"
 
 echo "GOOS: ${GOOS}"
 echo "GOARCH: ${GOARCH}"
-echo "GO_PATH: ${GO_PATH}"
+echo "GO_CUSTOM_PATH: ${GO_CUSTOM_PATH}"
 
 usage() {
     echo
-    echo "To use the newly installed go binary:"
-    echo "  - add ${GO_PATH}/bin to your PATH environment variable."
-    echo "  - set ${GO_PATH} as \$GOROOT environment"
+    echo "To use the installed go binary, add this to your .bashrc or .zshrc file:"
+    echo "      export PATH=\"${GO_CUSTOM_PATH}/bin:\$PATH\""
+    echo "      export GOROOT=\"${GO_CUSTOM_PATH}\""
+    echo "      export GOPATH=\"$(dirname $GO_CUSTOM_PATH)\""
 
     echo
     echo "To use go in the current terminal session, execute the following: "
-    echo "      export PATH=\"${GO_PATH}/bin:\$PATH\""
-    echo "      export GOROOT=\"${GO_PATH}\""
+    echo "      export PATH=\"${GO_CUSTOM_PATH}/bin:\$PATH\""
+    echo "      export GOROOT=\"${GO_CUSTOM_PATH}\""
+    echo "      export GOPATH=\"$(dirname $GO_CUSTOM_PATH)\""
 }
 
-if [ -d "${GO_PATH}" ]; then
+if [ -d "${GO_CUSTOM_PATH}" ] && [ "$GO_VERSION" = "$(cat $GO_CUSTOM_PATH/go-version)" ]; then
     echo "Go is already installed" && usage
     exit 0
 else
     echo "Downloading ${GO_SRC}..." && \
         mkdir -p "$(dirname $DL_PATH)" && \
         wget --progress=bar:force:noscroll -O "${DL_PATH}" "${GO_SRC}" && \
-        echo "Extracting ${GO_TAR} to ${GO_PATH}..." && \
-        mkdir -p "${GO_PATH}" && \
-        rm -rf ${GO_PATH} && tar -C $(dirname $GO_PATH) -xzf "${DL_PATH}" && \
-        echo "Installed Go ${GO_VERSION} to ${GO_PATH}" && usage
+        echo "Extracting ${GO_TAR} to ${GO_CUSTOM_PATH}..." && \
+        mkdir -p "${GO_CUSTOM_PATH}" && \
+        rm -rf ${GO_CUSTOM_PATH} && tar -C $(dirname $GO_CUSTOM_PATH) -xzf "${DL_PATH}" && \
+        echo $GO_VERSION > "$GO_CUSTOM_PATH/go-version" && \
+        echo "Installed Go ${GO_VERSION} to ${GO_CUSTOM_PATH}" && usage
 fi
 
 cd $WORKDIR
