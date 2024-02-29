3334041b9401198aa649a98a693df9f126c70db7
Adones Pitogo <pitogo.adones@gmail.com>
2024-02-29 14:41:10 +0800

    sdk: added comments to purchase object

--- sdk/api/payments/purchase.go
+++ sdk/api/payments/purchase.go
@@ -10,12 +10,33 @@ import "net/http"
 
 // Purchase represents a record in purchases table in the database.
 type Purchase interface {
+
+    // Returns the name of the purchase item.
 	Name() string
+
+    // Returns true if the purchase request has a fixed price.
 	FixedPrice() (float64, bool)
+
+    // Create a payment for the purchase.
 	CreatePayment(amount float64, optname string) error
+
+    // Pay using the customers wallet.
+    // The amount will be debitted from the wallet once the purchase request has been confirmed.
 	PayWithWallet(amount float64) error
+
+    // Returns the state of the purchase.
+    // The state includes the total accumulated payment for the purchase and other important details.
 	State() (PurchaseState, error)
+
+    // Executes the payment for the purchase.
+    // This will redirect the user to the callback URL of purchase request.
 	Execute(w http.ResponseWriter)
+
+    // Confirm the purchase.
+    // This must be executed in the purchase callback handler.
 	Confirm() error
+
+    // Cancel the purchase.
+    // This must be executed in the purchase callback handler.
 	Cancel() error
 }
