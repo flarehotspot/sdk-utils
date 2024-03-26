ef9f2536699a835c23c64eb10571263531251795
vince <marc.bentoy@gmail.com>
2024-03-26 15:10:18 +0800

    feat: console and file logger

--- core/internal/plugins/logger-api.go
+++ core/internal/plugins/logger-api.go
@@ -31,67 +31,136 @@ const (
 )
 
 type LoggerApi struct {
-	debug *log.Logger
-	info  *log.Logger
-	error *log.Logger
+	debugLogger *log.Logger
+	infoLogger  *log.Logger
+	errorLogger *log.Logger
 }
 
+const (
+	Logdir      = "./flarelogs"
+	Logfilename = "app.log"
+	Infoprefix  = "[INFO] "
+	Debugprefix = "[DEBUG] "
+	Errorprefix = "[ERROR] "
+)
+
 func NewLoggerApi() *LoggerApi {
+	// run a go routine for log rotation
+
+	// run a go routine for log retention
+
 	return &LoggerApi{
-		debug: log.New(os.Stdout, colorize(yellow, "flarelog [DEBUG]: "), log.LstdFlags|log.Lshortfile),
-		info:  log.New(os.Stdout, colorize(lightBlue, "flarelog [INFO]: "), log.LstdFlags|log.Lshortfile),
-		error: log.New(os.Stdout, colorize(lightRed, "flarelog [ERROR]: "), log.LstdFlags|log.Lshortfile),
+		debugLogger: log.New(os.Stdout, "", log.LstdFlags|log.Llongfile),
+		infoLogger:  log.New(os.Stdout, "", log.LstdFlags|log.Llongfile),
+		errorLogger: log.New(os.Stdout, "", log.LstdFlags|log.Llongfile),
 	}
 }
 
 func openLogFile(path string) (*os.File, error) {
-	logFile, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	// ensure log file directory exists
+	err := os.MkdirAll(Logdir, 0700)
 	if err != nil {
+		log.Fatal("Error creating log directory: ", err)
+		return nil, err
+	}
+
+	// opening/creating file
+	logFile, err := os.OpenFile(Logdir+"/"+path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal("Error creating log file: ", err)
 		return nil, err
 	}
 
 	return logFile, nil
 }
 
-func (self *LoggerApi) Debug(msg string) error {
-	// log to console
-	self.debug.Output(2, msg)
+func logToConsole(l *log.Logger, msg string) error {
+	// set output to console
+	l.SetOutput(os.Stdout)
 
-	// log to log file
-	// open file
-	file, err := openLogFile("./logs.log")
+	// log to console
+	l.SetPrefix(colorize(yellow, Infoprefix))
+	err := l.Output(2, msg)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error logging to console", err)
+		return err
 	}
 
-	log.SetOutput(file)
-	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+	return nil
+}
 
-	log.Println()
+func logToFile(l *log.Logger, msg string) error {
+	// open file
+	file, err := openLogFile(Logfilename)
+	if err != nil {
+		log.Fatal("Error opening log file: ", err)
+		return err
+	}
 
-	// write logs to file
+	// set output to file
+	l.SetOutput(file)
+
+	// log to file
+	l.SetPrefix(Debugprefix)
+	err = l.Output(2, msg)
+	if err != nil {
+		log.Fatal("Error logging to file", err)
+		return err
+	}
+
+	return nil
+}
+
+func (self *LoggerApi) Debug(msg string) error {
+	// log to file
+	err := logToFile(self.debugLogger, msg)
+	if err != nil {
+		log.Fatal("Error logging to file: ", err)
+		return err
+	}
+
+	// log to console
+	err = logToConsole(self.debugLogger, msg)
+	if err != nil {
+		log.Fatal("Error logging to console: ", err)
+		return err
+	}
 
 	return nil
 }
 
 func (self *LoggerApi) Info(msg string) error {
-	// log to console
-	self.info.Output(2, msg)
+	// log to file
+	err := logToFile(self.infoLogger, msg)
+	if err != nil {
+		log.Fatal("Error logging to file: ", err)
+		return err
+	}
 
-	// log to log file
-	// open file
-	// write logs to file
+	// log to console
+	err = logToConsole(self.infoLogger, msg)
+	if err != nil {
+		log.Fatal("Error logging to console: ", err)
+		return err
+	}
 
 	return nil
 }
 
 func (self *LoggerApi) Error(msg string) error {
-	// log to console
-	self.error.Output(2, msg)
+	// log to file
+	err := logToFile(self.errorLogger, msg)
+	if err != nil {
+		log.Fatal("Error logging to file: ", err)
+		return err
+	}
 
-	// log to log file
-	// open file
-	// write logs to file
+	// log to console
+	err = logToConsole(self.errorLogger, msg)
+	if err != nil {
+		log.Fatal("Error logging to console: ", err)
+		return err
+	}
 
 	return nil
 }
@@ -99,3 +168,6 @@ func (self *LoggerApi) Error(msg string) error {
 func colorize(colorCode int, v string) string {
 	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
 }
+
+// TODO: read logs
+// func readLogs() {}
