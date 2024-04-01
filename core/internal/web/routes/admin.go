c4f34f45ad5466fd658316202848e961e9b18dde
Adones Pitogo <pitogo.adones@gmail.com>
2024-04-01 15:18:45 +0800

    clean up theme picker

--- core/internal/web/routes/admin.go
+++ core/internal/web/routes/admin.go
@@ -13,34 +13,29 @@ func AdminRoutes(g *plugins.CoreGlobals) {
 	rootR := router.RootRouter
 	adminR := g.CoreAPI.HttpAPI.HttpRouter().AdminRouter()
 
-	getAvailableThemes := controllers.RespondJsonThemes(g)
-	saveThemesAvailable := controllers.SaveThemeSettings(g)
-
 	adminIndexCtrl := controllers.AdminIndexPage(g)
 	adminSseCtrl := controllers.AdminSseHandler(g)
 
 	rootR.Handle("/admin", adminIndexCtrl).Methods("GET").Name(routenames.RouteAdminIndex)
 	adminR.Get("/events", adminSseCtrl).Name(routenames.RouteAdminSse)
+	adminR.Post("/themes", controllers.SaveThemeSettings(g)).Name(routenames.RouteAdminThemes)
 
-	adminR.Group("/themes", func(subrouter sdkhttp.HttpRouterInstance) {
-		subrouter.Get("/", getAvailableThemes).Name(routenames.AdminThemeSettings)
-		subrouter.Post("/save", saveThemesAvailable).Name(routenames.AdminSaveThemeSettings)
-	})
+	g.CoreAPI.HttpAPI.VueRouter().RegisterAdminRoutes([]sdkhttp.VueAdminRoute{
+		{
+			RouteName:   "theme-picker",
+			RoutePath:   "/theme-picker",
+			HandlerFunc: controllers.GetAvailableThemes(g),
+			Component:   "admin/ThemePicker.vue",
+		},
+	}...)
+
 	g.CoreAPI.HttpAPI.VueRouter().AdminNavsFunc(func(acct sdkacct.Account) []sdkhttp.VueAdminNav {
 		return []sdkhttp.VueAdminNav{
 			{
 				Category:  sdkhttp.NavCategoryThemes,
-				Label:     "Theme Picker",
-				RouteName: "settings-theme",
+				Label:     "Select Theme",
+				RouteName: "theme-picker",
 			},
 		}
 	})
-	g.CoreAPI.HttpAPI.VueRouter().RegisterAdminRoutes([]sdkhttp.VueAdminRoute{
-		{
-			RouteName:   "settings-theme",
-			RoutePath:   "/themes",
-			HandlerFunc: getAvailableThemes,
-			Component:   "ThemeSettings.vue",
-		},
-	}...)
 }
--- core/internal/web/routes/names/admin.go
+++ core/internal/web/routes/names/admin.go
@@ -1,10 +1,9 @@
 package routenames
 
 const (
-	AdminThemeLayout       = "admin.theme.layout"
-	AdminThemeIndex        = "admin.theme.index"
-	AdminThemeLogin        = "admin.theme.login"
-	AdminNavs              = "admin.navs"
-	AdminThemeSettings     = "admin.theme.settings"
-	AdminSaveThemeSettings = "admin.theme.save"
+	AdminThemeLayout = "admin.theme.layout"
+	AdminThemeIndex  = "admin.theme.index"
+	AdminThemeLogin  = "admin.theme.login"
+	AdminNavs        = "admin.navs"
+	RouteAdminThemes = "admin.themes.save"
 )
