fdcf715068e80c9aa643a6df8888b3eeb0b4dc52
Adones Pitogo <pitogo.adones@gmail.com>
2024-04-03 15:46:55 +0800

    docs: translations example

--- core/internal/web/routes/portal.go
+++ core/internal/web/routes/portal.go
@@ -4,7 +4,9 @@ import (
 	"github.com/flarehotspot/core/internal/plugins"
 	"github.com/flarehotspot/core/internal/web/controllers"
 	"github.com/flarehotspot/core/internal/web/router"
-	"github.com/flarehotspot/core/internal/web/routes/names"
+	routenames "github.com/flarehotspot/core/internal/web/routes/names"
+	sdkconnmgr "github.com/flarehotspot/sdk/api/connmgr"
+	sdkhttp "github.com/flarehotspot/sdk/api/http"
 )
 
 func PortalRoutes(g *plugins.CoreGlobals) {
@@ -18,4 +20,19 @@ func PortalRoutes(g *plugins.CoreGlobals) {
 	rootR.Handle("/", portalIndexCtrl).Methods("GET").Name(routenames.RoutePortalIndex)
 	portalR.Get("/events", portalSseCtrl).Name(routenames.RoutePortalSse)
 	portalR.Get("/nav/items", portalItemsCtrl).Name(routenames.PortalItems)
+
+	g.CoreAPI.HttpAPI.VueRouter().RegisterPortalRoutes(sdkhttp.VuePortalRoute{
+		RouteName: "portal.sample.2",
+		RoutePath: "/portal/sample",
+        Component: "portal/Sample.vue",
+	})
+
+	g.CoreAPI.HttpAPI.VueRouter().PortalItemsFunc(func(clnt sdkconnmgr.ClientDevice) []sdkhttp.VuePortalItem {
+		return []sdkhttp.VuePortalItem{
+			{
+                RouteName: "portal.sample.2",
+                Label: "Sample Vue Component",
+            },
+		}
+	})
 }
