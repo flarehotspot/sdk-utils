d6306a95a9aa8222585534873545cc4780b9c144
Adones Pitogo <pitogo.adones@gmail.com>
2024-04-03 15:48:07 +0800

    core: remove sample vue

--- core/internal/web/routes/portal.go
+++ core/internal/web/routes/portal.go
@@ -5,8 +5,6 @@ import (
 	"github.com/flarehotspot/core/internal/web/controllers"
 	"github.com/flarehotspot/core/internal/web/router"
 	routenames "github.com/flarehotspot/core/internal/web/routes/names"
-	sdkconnmgr "github.com/flarehotspot/sdk/api/connmgr"
-	sdkhttp "github.com/flarehotspot/sdk/api/http"
 )
 
 func PortalRoutes(g *plugins.CoreGlobals) {
@@ -20,19 +18,4 @@ func PortalRoutes(g *plugins.CoreGlobals) {
 	rootR.Handle("/", portalIndexCtrl).Methods("GET").Name(routenames.RoutePortalIndex)
 	portalR.Get("/events", portalSseCtrl).Name(routenames.RoutePortalSse)
 	portalR.Get("/nav/items", portalItemsCtrl).Name(routenames.PortalItems)
-
-	g.CoreAPI.HttpAPI.VueRouter().RegisterPortalRoutes(sdkhttp.VuePortalRoute{
-		RouteName: "portal.sample.2",
-		RoutePath: "/portal/sample",
-        Component: "portal/Sample.vue",
-	})
-
-	g.CoreAPI.HttpAPI.VueRouter().PortalItemsFunc(func(clnt sdkconnmgr.ClientDevice) []sdkhttp.VuePortalItem {
-		return []sdkhttp.VuePortalItem{
-			{
-                RouteName: "portal.sample.2",
-                Label: "Sample Vue Component",
-            },
-		}
-	})
 }
