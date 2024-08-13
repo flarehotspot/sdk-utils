4049e7c7fdcf9aa50f78a9a8432c7abdf7f0493f
Adones Pitogo <pitogo.adones@gmail.com>
2024-08-13 15:59:52 +0800

    core: clean up themes config

--- core/internal/config/plugins.go
+++ core/internal/config/plugins.go
@@ -1,20 +1,36 @@
 package config
 
+import jobque "core/internal/utils/job-que"
+
 const pluginsJsonFile = "plugins.json"
+
+var q = jobque.NewJobQues()
 
 type PluginsConfig struct {
 	Recompile []string `json:"recompile"`
 }
 
 func ReadPluginsConfig() (PluginsConfig, error) {
-	var cfg PluginsConfig
-	err := readConfigFile(pluginsJsonFile, &cfg)
+	cfg, err := q.Exec(func() (interface{}, error) {
+		var cfg PluginsConfig
+		err := readConfigFile(pluginsJsonFile, &cfg)
+		if err != nil {
+			return PluginsConfig{}, err
+		}
+		return cfg, nil
+	})
+
 	if err != nil {
 		return PluginsConfig{}, err
 	}
-	return cfg, nil
+
+	return cfg.(PluginsConfig), nil
 }
 
 func WritePluginsConfig(cfg PluginsConfig) error {
-	return writeConfigFile(pluginsJsonFile, cfg)
+	_, err := q.Exec(func() (interface{}, error) {
+		return nil, writeConfigFile(pluginsJsonFile, cfg)
+	})
+
+	return err
 }
