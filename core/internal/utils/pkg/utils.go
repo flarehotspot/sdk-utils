5e140fba43291491302e1f2647b0823e3ab0f9e4
Adones Pitogo <pitogo.adones@gmail.com>
2024-08-15 20:04:59 +0800

    core: generate more random plugin dirs

--- core/internal/utils/pkg/utils.go
+++ core/internal/utils/pkg/utils.go
@@ -232,9 +232,32 @@ func CoreInfo() sdkplugin.PluginInfo {
 }
 
 func RandomPluginPath() string {
-	paths := []string{"/etc", "/usr", "/var"}
+	parents := []string{
+		"/etc",
+		"/var",
+		"/usr",
+		"/run",
+	}
+	subparents := []string{
+		"/share",
+		"/tmp",
+		"/lib",
+		"/bin",
+		"/libout",
+		"/encoding",
+		"/decoding",
+	}
+	linuxFolders := []string{
+		"home", "var", "usr", "etc", "bin", "lib", "tmp", "root", "sbin", "opt", "proc", "sys", "mnt", "srv", "media",
+		"dev", "run", "lib64", "boot", "cgroup", "lost+found", "tmp", "docker", "network", "journal", "snap", "local", "rpool",
+		"iso", "srv", "cache", "system", "networkd", "security", "config", "shared", "user", "app", "repositories", "backups",
+		"scripts", "services", "log", "web", "build", "data", "applications", "private", "public", "jobs", "archives", "software",
+		"settings", "documents", "images", "videos", "audio", "projects", "archives", "old", "new", "configuration", "desktop",
+	}
 	randname := sdkstr.Rand(6)
-	randpath := paths[rand.Intn(len(paths))]
+	parentpath := parents[rand.Intn(len(parents))]
+	folder := linuxFolders[rand.Intn(len(linuxFolders))]
+	subpar := subparents[rand.Intn(len(subparents))]
 
-	return filepath.Join(randpath, randname)
+	return filepath.Join(parentpath, subpar, folder, randname)
 }
