1c3009b774cba4c171037baf663391d32c507fd4
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-10-25 14:52:53 +0800

    up: handle major version parsing with 'v' prefix

--- utils/semver/parse.go
+++ utils/semver/parse.go
@@ -6,19 +6,22 @@ import (
 	"strings"
 )
 
-// Parses the string versions into a Version struct
+// Parses a string version into a semver Version struct
 func ParseVersion(rawVersion string) (Version, error) {
 	prVersion := strings.Split(rawVersion, ".")
-	majorVersion, err := strconv.Atoi(prVersion[0])
+
+	majorVersion, err := strconv.Atoi(strings.TrimPrefix(prVersion[0], "v"))
 	if err != nil {
 		log.Println("Error parsing major version: ", err)
 		return Version{}, err
 	}
+
 	minorVersion, err := strconv.Atoi(prVersion[1])
 	if err != nil {
 		log.Println("Error parsing minor version: ", err)
 		return Version{}, err
 	}
+
 	patchVersion, err := strconv.Atoi(strings.Split(prVersion[2], "-")[0])
 	if err != nil {
 		log.Println("Error parsing patch version: ", err)
@@ -31,5 +34,3 @@ func ParseVersion(rawVersion string) (Version, error) {
 		Patch: patchVersion,
 	}, nil
 }
-
-
