83c093937edb325d7de944685150a234a756aae4
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-11-08 15:14:07 +0800

    Revert "remove com.flarego.default-theme"
    This reverts commit 21f3192c63b6af26e5400f4cc8bb013e8a05b062.

---
+++ plugins/system/com.flarego.default-theme/app/routes.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	sdkplugin "sdk/api/plugin"
+
+	"com.flarego.default-theme/app/controllers"
+)
+
+const (
+	RouteNameLogin   = "auth.login"
+	RouteNameLogout  = "auth.logout"
+	RoutePortalItems = "portal.items"
+	RouteAdminNavs   = "admin.navs"
+	RoutePayments    = "save.settings"
+)
+
+func SetupRoutes(api sdkplugin.PluginApi) {
+	// pluginRouter := api.Http().HttpRouter().PluginRouter()
+	adminRouter := api.Http().HttpRouter().AdminRouter()
+	// pluginRouter.Get("/test", controllers.IndexCtrl(api)).Name("index")
+	adminRouter.Get("/test", controllers.TestCtrl(api)).Name("test")
+	// pluginRouter.Group("/auth", func(subrouter sdkhttp.HttpRouterInstance) {
+	// 	subrouter.Post("/login", controllers.LoginCtrl(api)).Name(RouteNameLogin)
+	// 	subrouter.Post("/logout", controllers.LogoutCtrl(api)).Name(RouteNameLogout)
+	// })
+
+	// adminRouter := api.Http().HttpRouter().AdminRouter()
+	// adminRouter.Get("/navs", controllers.GetAdminNavs(api)).Name(RouteAdminNavs)
+
+}
---
+++ plugins/system/com.flarego.default-theme/app/themes/admin.go
@@ -0,0 +1,64 @@
+package themes
+
+import (
+	// "net/http"
+
+	"net/http"
+	sdkhttp "sdk/api/http"
+	plugin "sdk/api/plugin"
+
+	"com.flarego.default-theme/resources/views/admin"
+	"github.com/a-h/templ"
+)
+
+func SetAdminTheme(api plugin.PluginApi) {
+	api.Themes().NewAdminTheme(sdkhttp.AdminThemeOpts{
+		JsFile:  "theme.js",
+		CssFile: "theme.css",
+		CssLib:  sdkhttp.CssLibBootstrap5,
+		LayoutFactory: func(w http.ResponseWriter, r *http.Request, data sdkhttp.AdminLayoutData) templ.Component {
+			layout := admin.AdminLayout(api, data)
+			return layout
+		},
+		IndexPageFactory: func(w http.ResponseWriter, r *http.Request) sdkhttp.ViewPage {
+			page := admin.AdminIndexPage()
+			return sdkhttp.ViewPage{PageContent: page}
+		},
+	})
+
+	api.Http().Navs().AdminNavsFactory(func(r *http.Request) []sdkhttp.AdminNavItemOpt {
+		return []sdkhttp.AdminNavItemOpt{
+			{
+				Label:     "Test",
+				Category:  sdkhttp.NavCategorySystem,
+				RouteName: "test",
+			},
+		}
+	})
+
+	// api.Themes().NewAdminTheme(themes.AdminTheme{
+	// 	CssLib: themes.CssLibBootstrap4,
+	// 	DashboardComponent: themes.ThemeComponent{
+	// 		RouteName: "dashboard",
+	// 		Component: "admin/Dashboard.vue",
+	// 	},
+	// 	LayoutComponent: themes.ThemeComponent{
+	// 		Component: "admin/ThemeLayout.vue",
+	// 	},
+	// 	LoginComponent: themes.ThemeComponent{
+	// 		RouteName: "login",
+	// 		Component: "admin/ThemeLogin.vue",
+	// 	},
+	// 	ThemeAssets: &themes.ThemeAssets{
+	// 		Scripts: []string{
+	// 			"vendor/polyfills/intersection-observer.js",
+	// 			"vendor/polyfills/intersection-observer-enable-polling.js",
+	// 			"vendor/bootstrap-vue/bootstrap-vue-2.23.1.umd.min.js",
+	// 			"vendor/bootstrap-vue/bootstrap-vue-icons-2.23.1.umd.min.js",
+	// 		},
+	// 		Styles: []string{
+	// 			"vendor/bootstrap-4.6.1/bootstrap.min.css",
+	// 		},
+	// 	},
+	// })
+}
